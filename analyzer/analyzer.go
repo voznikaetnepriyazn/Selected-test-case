e03ad607a67baff0b192833c6102b3fac4c4b315
Daria Maslova <maslovadaria16.07@gmail.com>
2026-03-13 21:39:36 +0300

    skelet

---
+++ analyzer/analyzer.go
@@ -0,0 +1,24 @@
+package analyzer
+
+import (
+	"analysis"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name: "loganalyze",
+	Doc:  "check logs for matching all rules",
+	Run:  run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	switch {
+	case:
+
+	case:
+
+	case:
+
+	case:
+
+	}
+}
