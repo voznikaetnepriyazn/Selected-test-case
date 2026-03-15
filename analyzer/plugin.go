49aed6c45c93c2c84f3e6ce8576d00a099abe629
Daria Maslova <maslovadaria16.07@gmail.com>
2026-03-15 20:23:44 +0300

    piagin

---
+++ analyzer/plugin.go
@@ -0,0 +1,4 @@
+package analyzer
+
+// AnalyzerPlugin — точка входа для golangci-lint плагина
+var AnalyzerPlugin = Analyzer
--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	_ "testcase/analyzer"
+
 	"golang.org/x/tools/go/analysis/passes/findcall"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
